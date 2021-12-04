c69fabd7a26841015ba8aadbfa660deb334653ae
Edvard Thörnros <edvard.thornros@gmail.com>
2021-12-05 00:32:34 +0100

    Refactor day 4

--- src/Day04.purs
+++ src/Day04.purs
@@ -1,30 +1,25 @@
 module Day04 where
 
 import Prelude
+import Data.Array (catMaybes, mapWithIndex, slice, (!!), fromFoldable, (:), drop, (..))
+import Data.Array as Array
+import Data.Either (Either(..))
+import Data.Foldable (sum, minimum, maximum)
+import Data.Int (fromString)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String.CodeUnits (fromCharArray)
 import Effect (Effect)
 import Effect.Console (log)
-import Data.Either (Either(..))
-import Node.FS.Async as FS
 import Node.Encoding (Encoding(UTF8))
+import Node.FS.Async as FS
 import Partial.Unsafe (unsafeCrashWith)
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.String.Utils (lines, words, toCharArray)
-import Data.String.Common (joinWith)
-import Data.Int (fromString)
-import Data.Array (catMaybes, mapWithIndex, slice, (!!), fromFoldable, (:), drop)
-import Data.Array as Array
-import Data.Tuple (fst, snd, Tuple(..))
-import Data.String.CodeUnits (fromCharArray)
-import Data.Function (identity)
-import Data.Foldable (sum)
-import Debug (spy)
 import Text.Parsing.Parser as P
 import Text.Parsing.Parser.Combinators as PC
 import Text.Parsing.Parser.String as PS
 import Text.Parsing.Parser.Token as PT
 
-number :: P.Parser String Int
-number = do
+numberParser :: P.Parser String Int
+numberParser = do
   PS.skipSpaces
   digits <- PC.many1 PT.digit
   pure $ fromFoldable digits
@@ -41,13 +36,13 @@ repeatParser n parser =
   else do
     pure []
 
-board :: P.Parser String (Array Int)
-board = PC.try $ repeatParser (5 * 5) number
+boardParser :: P.Parser String (Array Int)
+boardParser = PC.try $ repeatParser (5 * 5) numberParser
 
 inputParser :: P.Parser String Input
 inputParser = do
-  numbers <- number `PC.sepBy` PS.string ","
-  boards <- PC.many1 board
+  numbers <- numberParser `PC.sepBy` PS.string ","
+  boards <- PC.many1 boardParser
   pure
     $ { numbers: numbers # fromFoldable
       , boards: boards # fromFoldable
@@ -66,44 +61,50 @@ parse = case _ of
     Right o -> o
 
 takeEvery :: Int -> Array Int -> Array Int
-takeEvery n = mapWithIndex (\i b -> if 0 == mod i n then Just b else Nothing) >>> catMaybes
+takeEvery n =
+  let
+    multipleOrNothing index value =
+      if 0 == mod index n then
+        Just value
+      else
+        Nothing
+  in
+    mapWithIndex multipleOrNothing >>> catMaybes
 
 allLines :: Array Int -> Array (Array Int)
 allLines b =
-  [ slice 0 5 b
-  , slice 5 10 b
-  , slice 10 15 b
-  , slice 15 20 b
-  , slice 20 25 b
-  ]
-    <> [ takeEvery 5 (drop 0 b)
-      , takeEvery 5 (drop 1 b)
-      , takeEvery 5 (drop 2 b)
-      , takeEvery 5 (drop 3 b)
-      , takeEvery 5 (drop 4 b)
-      ]
+  let
+    sliceN n = slice n5 (n5 + 5) b
+      where
+      n5 = n * 5
+
+    takeN n = takeEvery 5 (drop n b)
+  in
+    (0 .. 4 <#> sliceN) <> (0 .. 4 <#> takeN)
 
 getIndex :: Array Int -> Int -> Maybe Int
 getIndex l e = Array.elemIndex e l
 
 score :: Input -> Array Int -> Int -> Int
 score i boardsBingoTurn turn =
-      let
-        number = i.numbers !! turn # fromMaybe 0
-        picked = Array.take (turn + 1) i.numbers
-        index = getIndex boardsBingoTurn turn # fromMaybe 0
-        board = i.boards !! index # fromMaybe []
-        boardAfter = board # Array.filter (\e -> Array.notElem e picked)
-        totalSum = boardAfter # sum
-      in
-        totalSum * number
+  fromMaybe 0 do
+    number <- i.numbers !! turn
+    let
+      picked = Array.take (turn + 1) i.numbers
+    index <- getIndex boardsBingoTurn turn
+    board <- i.boards !! index
+    let
+      boardAfter = board # Array.filter (\e -> Array.notElem e picked)
+    pure $ (boardAfter # sum) * number
 
-largest = Array.foldl max 0
+largest :: Array Int -> Int
+largest = maximum >>> fromMaybe 0
 
-smallest = Array.foldl min 99999 -- :/
+smallest :: Array Int -> Int
+smallest = minimum >>> fromMaybe 0
 
 -- Too low: 734
-solve :: Input -> Effect _
+solve :: Input -> Effect Unit
 solve i =
   let
     boardsBingoTurn :: Array Int
@@ -115,14 +116,14 @@ solve i =
         <#> smallest
 
     winning = score i boardsBingoTurn (smallest boardsBingoTurn)
-    loosing = score i boardsBingoTurn (largest boardsBingoTurn)
 
+    loosing = score i boardsBingoTurn (largest boardsBingoTurn)
   in
     do
       log "Day 04"
       log $ show winning
       log $ show loosing
 
-main :: _
+main :: Effect Unit
 main = do
   FS.readTextFile UTF8 "input/d04.txt" (parse >>> solve)
