51e8ff2fe9b40350c857806ad8658e8388de9c27
Edvard Thörnros <edvard.thornros@gmail.com>
2021-12-08 17:02:30 +0100

    Finish day8

--- src/Day08.purs
+++ src/Day08.purs
@@ -6,7 +6,10 @@ import Data.Array as A
 import Data.Either (Either(..))
 import Data.Foldable (sum)
 import Data.Int (fromString)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
+import Data.Map as M
+import Data.Set as S
 import Data.String.CodeUnits (fromCharArray)
 import Data.Ord (abs)
 import Effect (Effect)
@@ -29,15 +32,15 @@ repeatParser n parser =
   else do
     pure []
 
-wordParser :: P.Parser String (Array Char)
+wordParser :: P.Parser String (S.Set Char)
 wordParser = do
   PS.skipSpaces
   letters <- PC.many1 PT.letter
-  pure $ fromFoldable letters
+  pure $ S.fromFoldable letters
 
 type Line =
-  { every :: Array (Array Char)
-  , found :: Array (Array Char)
+  { every :: Array (S.Set Char)
+  , found :: Array (S.Set Char)
   }
 
 lineParser :: P.Parser String Line
@@ -63,23 +66,92 @@ parse = case _ of
     Left err -> unsafeCrashWith $ P.parseErrorMessage err
     Right o -> o
 
-countRelevantP1 :: forall t. Array (Array t) -> Int
-countRelevantP1 xs = xs <#> matches # sum
-    where matches x = case A.length x of
-                            2 -> 1
-                            3 -> 1
-                            4 -> 1
-                            7 -> 1
-                            _ -> 0
+countSimple xs = xs <#> matches # sum
+  where
+  matches x = case S.size x of
+    2 -> 1
+    3 -> 1
+    4 -> 1
+    7 -> 1
+    _ -> 0
+
+solveSimple :: S.Set Char -> Maybe Int
+solveSimple x =
+  case S.size x of
+    2 -> Just 1
+    3 -> Just 7
+    4 -> Just 4
+    7 -> Just 8
+    _ -> Nothing
+
+combine as bs = A.zip as bs <#> merge
+  where
+  merge (Tuple (Tuple x a) (Tuple _ b)) =
+    ( Tuple x case a, b of
+        Nothing, Just _ -> b
+        _, _ -> a
+    )
+
+isA :: forall t. Int -> (Tuple t (Maybe Int) -> Boolean)
+isA n = \(Tuple _ x) -> x == Just n
+
+solve960 :: Array (Tuple (S.Set Char) (Maybe Int)) -> (S.Set Char) -> Maybe Int
+solve960 known x = do
+  _ <- case S.size x of
+    6 -> Just unit
+    _ -> Nothing
+  Tuple four _ <- A.find (isA 4) known
+  Tuple seven _ <- A.find (isA 7) known
+  if S.subset four x then Just 9
+  else if S.subset seven x then Just 0
+  else Just 6
+
+solve235 :: Array (Tuple (S.Set Char) (Maybe Int)) -> (S.Set Char) -> Maybe Int
+solve235 known x = do
+  _ <- case S.size x of
+    5 -> Just unit
+    _ -> Nothing
+  Tuple one _ <- A.find (isA 1) known
+  Tuple four _ <- A.find (isA 4) known
+  Tuple eight _ <- A.find (isA 8) known
+  if S.subset one x then Just 3
+  else if S.subset (S.difference eight four) x then Just 2
+  else Just 5
+
+figureOutWires :: Array (S.Set Char) -> Array (Tuple (S.Set Char) (Maybe Int))
+figureOutWires x =
+  let
+    r f = A.zip x $ (x <#> f)
+    a = r solveSimple
+    b = combine a (r (solve960 a))
+    c = combine b (r (solve235 b))
+  in
+    c
+translate :: Tuple (Array (Tuple (S.Set Char) (Maybe Int))) (Array (S.Set Char)) -> Array Int
+translate (Tuple known xs) = xs
+    <#> (\x -> A.findMap (\(Tuple a b) -> if a == x then b else Nothing) known)
+    <#> fromMaybe 0
+
+parseDec :: Array Int -> Int
+parseDec = A.foldl (\i s -> 10 * i + s) 0
 
 solve :: Input -> Effect Unit
 solve i =
   let
-    _ = spy "_" $ 0
+    known :: Array (Array (Tuple (S.Set Char) (Maybe Int)))
+    known = i <#> _.every <#> figureOutWires
+
+    total = i
+        <#> _.found # A.zip known
+        <#> translate >>> parseDec
+        # sum
+    -- TODO, find the numbers (len and match), parse numbers
   in
     do
       log "Day 08"
-      log $ show $ i <#> (_.found >>> countRelevantP1) # sum
+      log $ show $ i <#> (_.found >>> countSimple) # sum
+      log $ show $ total
+
 
 main :: Effect Unit
 main = do
