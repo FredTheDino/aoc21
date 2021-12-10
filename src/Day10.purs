f3b91fbd2f7d50a8b1b354fef1a07c332e427b9f
Edvard Thörnros <edvard.thornros@gmail.com>
2021-12-11 00:45:47 +0100

    Solve day 10

--- src/Day10.purs
+++ src/Day10.purs
@@ -9,6 +9,7 @@ import Data.Int (fromString)
 import Data.Maybe (Maybe(..), fromMaybe, isJust)
 import Data.Tuple (Tuple(..))
 import Data.Ord (abs)
+import Data.Eq (eq)
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(UTF8))
@@ -18,6 +19,7 @@ import Data.Foldable (minimum, sum, product)
 import Data.String.Common as SC
 import Data.String.Pattern as SP
 import Data.String as S
+import Data.String.Utils as SU
 import Data.Function.Memoize (memoize)
 import Debug
 
@@ -26,7 +28,7 @@ type Input = Array String
 parse :: forall t. Either t String -> Input
 parse = case _ of
   Left _ -> unsafeCrashWith ":("
-  Right i -> SC.split (SP.Pattern "\n") i
+  Right i -> SC.split (SP.Pattern "\n") (SU.trimEnd i)
 
 isClose c = 0 /= score c
 
@@ -52,32 +54,72 @@ match o c = (openToClose o) == (Just c)
 
 eat line = { head: S.take 1 line, tail: S.drop 1 line }
 
-parseLine :: String -> String
-parseLine "" = ""
+parseLine :: String -> Either String String
+parseLine "" = Left ""
 parseLine line =
   let
     { head: open, tail: afterOpen } = eat line
-
-    inner afterOpen =
-      let
-        afterMiddle = parseLine afterOpen
-        { head: close, tail: afterClose } = eat afterMiddle
-      in
-        if not $ isClose close then
-          afterOpen
-        else if not $ match open close then
-          afterMiddle
-        else
-          parseLine afterClose
   in
-    if not $ isOpen open then line
-    else inner afterOpen
+    if not $ isOpen open then
+      Right line
+    else do
+      afterMiddle <- parseLine afterOpen
+      let { head: close, tail: afterClose } = eat afterMiddle
+      if not $ isClose close then
+        Right line
+      else if not $ match open close then
+        Left afterMiddle
+      else
+        parseLine afterClose
+
+rebuildLine :: String -> Either String String
+rebuildLine "" = Left ""
+rebuildLine line =
+  let
+    { head: open, tail: afterOpen } = eat line
+  in
+    if not $ isOpen open then
+      Right line
+    else do
+      afterMiddle <- case rebuildLine afterOpen of
+        Left s -> Left $ s <> (openToClose open # fromMaybe "")
+        Right s -> Right s
+      let { head: close, tail: afterClose } = eat afterMiddle
+      if not $ isClose close then
+        Right line
+      else if close == "" then
+        Left (openToClose open # fromMaybe "")
+      else
+        rebuildLine afterClose
+
+scoreRebuild = A.foldl
+  ( \s c -> s * 5.0 + case c of
+      ")" -> 1.0
+      "]" -> 2.0
+      "}" -> 3.0
+      ">" -> 4.0
+      _ -> 0.0
+  )
+  0.0
 
 solve :: Input -> Effect Unit
 solve i =
-  do
-    log "Day 10"
-    log $ show $ i <#> parseLine <#> S.take 1 <#> spy "error" <#> score # sum
+  let
+    incomplete = i # A.filter (parseLine >>> fromLeft "" >>> eq "")
+    rebuiltScores = incomplete <#>
+      ( rebuildLine
+          >>> fromLeft ""
+          >>> SU.toCharArray
+          >>> scoreRebuild
+      )
+    middle = (rebuiltScores # A.sort) !! ((A.length rebuiltScores) / 2)
+  in
+    do
+      log "Day 10"
+      log $ show $ i <#> parseLine <#> fromLeft "" <#> S.take 1 <#> score # sum
+      log $ show $ middle # fromMaybe 0.0
+
+-- 228508708 - to low
 
 main :: Effect Unit
 main = do
